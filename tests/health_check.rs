16048e40ac9e279d595de80673860f7862e3367b
Peter C. Norton <spacey-github.com@ssr.com>
2021-05-06 11:25:35 -0400

    Using the actix-web demo code.
    Run the rustfmt over it too.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 // tests/health-check.rs
 use std::net::TcpListener;
-use zero2prod::startup::{run};
+use zero2prod::startup::run;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
