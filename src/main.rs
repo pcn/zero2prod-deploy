b6f8571ab878c032b7e9018c16d90e8da2b0f152
Peter C. Norton <spacey-github.com@ssr.com>
2021-05-07 22:30:17 -0400

    Finished 3.6/3.7

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@
 //     run(listener)?.await
 // }
 
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind to localhost port 8000");
+    let listener =
+        TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to localhost port 8000");
     run(listener)?.await
 }
