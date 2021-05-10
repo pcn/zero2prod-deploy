c51368afef0ba07eaccb6c50eabb816735227c85
Peter C. Norton <spacey-github.com@ssr.com>
2021-05-10 10:49:14 -0400

    Add environment variables and support for SSL

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@
 //     run(listener)?.await
 // }
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -20,7 +20,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!(
