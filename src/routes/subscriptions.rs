ae44f4ac60ebcacce2fe954bf0d2a78835f702f1
Peter C. Norton <spacey-github.com@ssr.com>
2021-05-18 08:19:58 -0400

    Finish 7.9, still with prior errors

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::PgPool;
+use sqlx::{Postgres, PgPool, Transaction};
 // use tracing_futures::Instrument;
 use uuid::Uuid;
 use std::convert::TryInto;
@@ -48,11 +48,19 @@ pub async fn subscribe(
         .0
         .try_into()
         .map_err(|_| HttpResponse::BadRequest().finish())?;
-    let subscriber_id = insert_subscriber(&pool, &new_subscriber)
+    let mut transaction = pool
+        .begin()
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
     let subscription_token = generate_subscription_token();
-    store_token(&pool, subscriber_id, &subscription_token)
+    store_token(&mut transaction, subscriber_id, &subscription_token)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+    transaction
+        .commit()
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
     send_confirmation_email(&email_client, new_subscriber, &base_url.0, &subscription_token)
@@ -63,10 +71,10 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Store subscription token in the database",
-    skip(subscription_token, pool)
+    skip(subscription_token, transaction)
 )]
 pub async fn store_token(
-    pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     subscriber_id: Uuid,
     subscription_token: &str,
 ) -> Result<(), sqlx::Error> {
@@ -76,7 +84,7 @@ VALUES ($1, $2)"#,
         subscription_token,
         subscriber_id
     )
-        .execute(pool)
+        .execute(transaction)
         .await
         .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}", e);
@@ -152,10 +160,10 @@ pub fn is_valid_name(s: &str) -> bool {
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+    skip(new_subscriber, transaction)
 )]
 pub async fn insert_subscriber(
-    pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     new_subscriber: &NewSubscriber,
 ) -> Result<Uuid, sqlx::Error> {
     let subscriber_id = Uuid::new_v4();
@@ -169,7 +177,7 @@ VALUES ($1, $2, $3, $4, 'pending_confirmation')
         new_subscriber.name.as_ref(),
         Utc::now()
     )
-    .execute(pool)
+    .execute(transaction)
     .await
     .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
